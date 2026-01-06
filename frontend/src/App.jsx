43c46b9bb4066a984674d27124a7ecd8bb5d90c7
Kh Salar Hassan <khsalarhassan@gmail.com>
2026-01-06 15:00:11 +0600

    added navbar + POS design improved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,73 +1,82 @@
-import React, { useState, createContext, useContext, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate, Outlet } from 'react-router-dom'; // Import Outlet
-import Login from './components/Login';
-import Dashboard from './components/Dashboard';
-import Sidebar from './components/Sidebar';
+import React, { useState, createContext, useContext, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  Outlet,
+} from "react-router-dom"; // Import Outlet
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
+import Sidebar from "./components/Sidebar";
 
 // Import all new page components
-import AllProducts from './components/Products/AllProducts';
-import CreateProduct from './components/Products/CreateProduct';
-import EditProduct from './components/Products/EditProduct';
-import POS from './components/Sale/POS';
-import AllSales from './components/Sale/AllSales';
-import CreateSale from './components/Sale/CreateSale';
-import SaleReturn from './components/Sale/SaleReturn';
-import AllMaterials from './components/Materials/AllMaterials';
-import AddMaterial from './components/Materials/AddMaterial';
-import ScrapeMaterials from './components/Materials/ScrapeMaterials';
-import RecoverMaterials from './components/Materials/RecoverMaterials';
-import EditMaterial from './components/Materials/EditMaterial';
-import AllProductions from './components/Productions/AllProductions';
-import NewProduction from './components/Productions/NewProduction';
-import EditProduction from './components/Productions/EditProduction';
-import AllPurchase from './components/Purchase/AllPurchase';
-import NewPurchase from './components/Purchase/NewPurchase';
-import AllSupplier from './components/Purchase/AllSupplier';
-import AddSupplier from './components/Purchase/AddSupplier';
-import AllFactory from './components/Factory/AllFactory';
-import AddFactory from './components/Factory/AddFactory';
-import EditFactory from './components/Factory/EditFactory';
-import AllStore from './components/Stores/AllStore';
-import AddStore from './components/Stores/AddStore';
-import EditStore from './components/Stores/EditStore'; 
-import SaleReport from './components/Report/SaleReport';
-import PurchaseReport from './components/Report/PurchaseReport';
-import ProductionReport from './components/Report/ProductionReport';
-import WastageReport from './components/Report/WastageReport';
-import ScrapeReport from './components/Report/ScrapeReport';
-import AllUser from './components/Users/AllUser';
-import CreateUser from './components/Users/CreateUser';
-import Settings from './components/Settings/Settings';
-import AddShop from './components/Shop/AddShop';
-import AllShop from './components/Shop/AllShop';
-import AllReturns from './components/Sale/AllReturns';
-import StoreDetails from './components/Stores/StoreDetails';
-import StoreToShopTransfer from './components/Stores/StoreToShopTransfer';
-import TransferManagement from './components/Stores/TransferManagement';
-import AddTransfer from './components/Transfer/AddTransfer';
-import StoreToStore from './components/Transfer/StoreToStore';
-import StoreToFactory from './components/Transfer/StoreToFactory';
-import StoreToShop from './components/Transfer/StoreToShop';
-import FactoryToFactory from './components/Transfer/FactoryToFactory';
-import FactoryToStore from './components/Transfer/FactoryToStore';
-import FactoryToShop from './components/Transfer/FactoryToShop';
-import ShopToShop from './components/Transfer/ShopToShop';
-import ShopToStore from './components/Transfer/ShopToStore';
-import ShopToFactory from './components/Transfer/ShopToFactory';
+import AllProducts from "./components/Products/AllProducts";
+import CreateProduct from "./components/Products/CreateProduct";
+import EditProduct from "./components/Products/EditProduct";
+import POS from "./components/Sale/POS";
+import AllSales from "./components/Sale/AllSales";
+import CreateSale from "./components/Sale/CreateSale";
+import SaleReturn from "./components/Sale/SaleReturn";
+import AllMaterials from "./components/Materials/AllMaterials";
+import AddMaterial from "./components/Materials/AddMaterial";
+import ScrapeMaterials from "./components/Materials/ScrapeMaterials";
+import RecoverMaterials from "./components/Materials/RecoverMaterials";
+import EditMaterial from "./components/Materials/EditMaterial";
+import AllProductions from "./components/Productions/AllProductions";
+import NewProduction from "./components/Productions/NewProduction";
+import EditProduction from "./components/Productions/EditProduction";
+import AllPurchase from "./components/Purchase/AllPurchase";
+import NewPurchase from "./components/Purchase/NewPurchase";
+import AllSupplier from "./components/Purchase/AllSupplier";
+import AddSupplier from "./components/Purchase/AddSupplier";
+import AllFactory from "./components/Factory/AllFactory";
+import AddFactory from "./components/Factory/AddFactory";
+import EditFactory from "./components/Factory/EditFactory";
+import AllStore from "./components/Stores/AllStore";
+import AddStore from "./components/Stores/AddStore";
+import EditStore from "./components/Stores/EditStore";
+import SaleReport from "./components/Report/SaleReport";
+import PurchaseReport from "./components/Report/PurchaseReport";
+import ProductionReport from "./components/Report/ProductionReport";
+import WastageReport from "./components/Report/WastageReport";
+import ScrapeReport from "./components/Report/ScrapeReport";
+import AllUser from "./components/Users/AllUser";
+import CreateUser from "./components/Users/CreateUser";
+import Settings from "./components/Settings/Settings";
+import AddShop from "./components/Shop/AddShop";
+import AllShop from "./components/Shop/AllShop";
+import AllReturns from "./components/Sale/AllReturns";
+import StoreDetails from "./components/Stores/StoreDetails";
+import StoreToShopTransfer from "./components/Stores/StoreToShopTransfer";
+import TransferManagement from "./components/Stores/TransferManagement";
+import AddTransfer from "./components/Transfer/AddTransfer";
+import StoreToStore from "./components/Transfer/StoreToStore";
+import StoreToFactory from "./components/Transfer/StoreToFactory";
+import StoreToShop from "./components/Transfer/StoreToShop";
+import FactoryToFactory from "./components/Transfer/FactoryToFactory";
+import FactoryToStore from "./components/Transfer/FactoryToStore";
+import FactoryToShop from "./components/Transfer/FactoryToShop";
+import ShopToShop from "./components/Transfer/ShopToShop";
+import ShopToStore from "./components/Transfer/ShopToStore";
+import ShopToFactory from "./components/Transfer/ShopToFactory";
+import Navbar from "./components/Navbar";
+import Notifications from "./components/Notifications";
+import UserProfile from "./components/UserProfile";
 
 const AuthContext = createContext(null);
 
 export const AuthProvider = ({ children }) => {
-  const [token, setToken] = useState(localStorage.getItem('token'));
+  const [token, setToken] = useState(localStorage.getItem("token"));
 
   const login = (newToken) => {
     setToken(newToken);
-    localStorage.setItem('token', newToken);
+    localStorage.setItem("token", newToken);
   };
 
   const logout = () => {
     setToken(null);
-    localStorage.removeItem('token');
+    localStorage.removeItem("token");
   };
 
   return (
@@ -79,7 +88,8 @@ export const AuthProvider = ({ children }) => {
 
 export const useAuth = () => useContext(AuthContext);
 
-const PrivateRoute = () => { // This component will now render an Outlet
+const PrivateRoute = () => {
+  // This component will now render an Outlet
   const { token } = useAuth();
   return token ? <Outlet /> : <Navigate to="/login" />;
 };
@@ -87,10 +97,17 @@ const PrivateRoute = () => { // This component will now render an Outlet
 // Layout component for authenticated users
 const AuthenticatedLayout = () => {
   return (
-    <div style={{ display: 'flex' }}>
-      <Sidebar />
-      <div style={{ flexGrow: 1, padding: '20px' }}>
-        <Outlet /> {/* This is where nested routes will render */}
+    <div className="">
+      <div className="h-15 w-full mx-auto">
+          <Navbar />
+        </div>
+      <div className="flex">
+        <div>
+        <Sidebar />
+      </div>
+        <div style={{ flexGrow: 1, padding: "20px" }}>
+          <Outlet /> {/* This is where nested routes will render */}
+        </div>
       </div>
     </div>
   );
@@ -134,7 +151,10 @@ function App() {
             <Route path="/stores/add" element={<AddStore />} />
             <Route path="/stores/edit/:id" element={<EditStore />} />
             <Route path="/stores/details/:id" element={<StoreDetails />} />
-            <Route path="/stores/transfer/:id" element={<StoreToShopTransfer />} />
+            <Route
+              path="/stores/transfer/:id"
+              element={<StoreToShopTransfer />}
+            />
             <Route path="/shop/add" element={<AddShop />} />
             <Route path="/shop/all" element={<AllShop />} />
             <Route path="/report/sale" element={<SaleReport />} />
@@ -148,14 +168,31 @@ function App() {
             <Route path="/transfers" element={<TransferManagement />} />
             <Route path="/transfer/add" element={<AddTransfer />} />
             <Route path="/transfer/store-to-store" element={<StoreToStore />} />
-            <Route path="/transfer/store-to-factory" element={<StoreToFactory />} />
+            <Route
+              path="/transfer/store-to-factory"
+              element={<StoreToFactory />}
+            />
             <Route path="/transfer/store-to-shop" element={<StoreToShop />} />
-            <Route path="/transfer/factory-to-factory" element={<FactoryToFactory />} />
-            <Route path="/transfer/factory-to-store" element={<FactoryToStore />} />
-            <Route path="/transfer/factory-to-shop" element={<FactoryToShop />} />
+            <Route
+              path="/transfer/factory-to-factory"
+              element={<FactoryToFactory />}
+            />
+            <Route
+              path="/transfer/factory-to-store"
+              element={<FactoryToStore />}
+            />
+            <Route
+              path="/transfer/factory-to-shop"
+              element={<FactoryToShop />}
+            />
             <Route path="/transfer/shop-to-shop" element={<ShopToShop />} />
             <Route path="/transfer/shop-to-store" element={<ShopToStore />} />
-            <Route path="/transfer/shop-to-factory" element={<ShopToFactory />} />
+            <Route
+              path="/transfer/shop-to-factory"
+              element={<ShopToFactory />}
+            />
+            <Route path="/notifications" element={<Notifications />} />
+            <Route path="/userprofile" element={<UserProfile />} />
             {/* Default route for authenticated users */}
             <Route path="/" element={<Navigate to="/dashboard" replace />} />
           </Route>
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,76 @@
+
+import React, {useState} from "react";
+
+// react icons
+import {IoIosSearch} from "react-icons/io";
+import {CiMenuFries} from "react-icons/ci";
+import logo from "/bspLogo.png"
+import { Bell, CircleUserRound } from "lucide-react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    const [mobileSidebarOpen, setMobileSidebarOpen] = useState(false)
+
+    return (
+        <nav
+            className="flex items-center justify-between w-full relative dark:bg-black bg-white rounded-md pr-5 pl-10 py-[8px] mt-2 ml-1">
+
+            {/* logo */}
+            <img src={logo} alt="logo" className="w-[75px] "/>
+
+            {/* nav links */}
+            <ul className="items-center gap-[20px] text-[1rem] text-[#424242] md:flex hidden">
+
+                <li className="before:w-0 hover:before:w-full before:bg-[#3B9DF8] before:h-[2px] before:transition-all before:duration-300 before:absolute relative before:rounded-full before:bottom-[-2px] dark:text-[#abc2d3] hover:text-[#3B9DF8] transition-all duration-300 before:left-0 cursor-pointer capitalize">home</li>
+
+                <li className="before:w-0 hover:before:w-full before:bg-[#3B9DF8] before:h-[2px] before:transition-all before:duration-300 before:absolute relative before:rounded-full before:bottom-[-2px] dark:text-[#abc2d3] hover:text-[#3B9DF8] transition-all duration-300 before:left-0 cursor-pointer capitalize">features</li>
+
+                <li className="before:w-0 hover:before:w-full before:bg-[#3B9DF8] before:h-[2px] before:transition-all before:duration-300 before:absolute relative before:rounded-full before:bottom-[-2px] dark:text-[#abc2d3] hover:text-[#3B9DF8] transition-all duration-300 before:left-0 cursor-pointer capitalize">blogs</li>
+
+                <li className="before:w-0 hover:before:w-full before:bg-[#3B9DF8] before:h-[2px] before:transition-all before:duration-300 before:absolute relative before:rounded-full before:bottom-[-2px] dark:text-[#abc2d3] hover:text-[#3B9DF8] transition-all duration-300 before:left-0 cursor-pointer capitalize">shop</li>
+            </ul>
+
+            {/* action buttons */}
+            <div className="items-center gap-[10px] flex">
+                <Link to="/notifications">
+                <button
+                    className="py-[7px] text-[1rem] px-[16px] dark:text-[#abc2d3] rounded-full capitalize hover:text-[#3B9DF8] transition-all duration-300 sm:flex hidden"> <Bell />
+                </button>
+                </Link>
+                <Link to="/userprofile">
+                <button
+                    className="py-[7px] text-[1rem] px-[16px] rounded-full capitalize bg-[#3B9DF8] text-white hover:bg-blue-400 transition-all duration-300 sm:flex hidden"><CircleUserRound />
+                </button>
+                </Link>
+                <CiMenuFries
+                    className="text-[1.8rem] dark:text-[#abc2d3] mr-1 text-[#424242]c cursor-pointer md:hidden flex"
+                    onClick={() => setMobileSidebarOpen(!mobileSidebarOpen)}/>
+            </div>
+
+            {/* mobile sidebar */}
+            <aside
+                className={` ${mobileSidebarOpen ? "translate-x-0 opacity-100 z-20" : "translate-x-[200px] opacity-0 z-[-1]"} md:hidden bg-white p-4 text-center absolute top-[65px] dark:bg-slate-700 right-0 w-full sm:w-[50%] rounded-md transition-all duration-300`}>
+                <div className="relative mb-5">
+                    <input
+                        className="py-1.5 pr-4 dark:bg-slate-800 dark:text-[#abc2d3] dark:border-slate-900/50 w-full pl-10 rounded-full border border-gray-200 outline-none focus:border-[#3B9DF8]"
+                        placeholder="Search..."/>
+                    <IoIosSearch className="absolute dark:text-slate-400 top-[8px] left-3 text-gray-500 text-[1.3rem]"/>
+                </div>
+                <ul className="items-center gap-[20px] text-[1rem] text-gray-600 flex flex-col">
+
+                    <li className="before:w-0 hover:before:w-full before:bg-[#3B9DF8] before:h-[2px] before:transition-all before:duration-300 before:absolute relative before:rounded-full before:bottom-[-2px] dark:text-[#abc2d3] hover:text-[#3B9DF8] transition-all duration-300 before:left-0 cursor-pointer capitalize">home</li>
+
+                    <li className="before:w-0 hover:before:w-full before:bg-[#3B9DF8] before:h-[2px] before:transition-all before:duration-300 before:absolute relative before:rounded-full before:bottom-[-2px] dark:text-[#abc2d3] hover:text-[#3B9DF8] transition-all duration-300 before:left-0 cursor-pointer capitalize">features</li>
+
+                    <li className="before:w-0 hover:before:w-full before:bg-[#3B9DF8] before:h-[2px] before:transition-all before:duration-300 before:absolute relative before:rounded-full before:bottom-[-2px] dark:text-[#abc2d3] hover:text-[#3B9DF8] transition-all duration-300 before:left-0 cursor-pointer capitalize">blogs</li>
+
+                    <li className="before:w-0 hover:before:w-full before:bg-[#3B9DF8] before:h-[2px] before:transition-all before:duration-300 before:absolute relative before:rounded-full before:bottom-[-2px] dark:text-[#abc2d3] hover:text-[#3B9DF8] transition-all duration-300 before:left-0 cursor-pointer capitalize">shop</li>
+
+                </ul>
+            </aside>
+        </nav>
+    );
+};
+
+export default Navbar;
+          
