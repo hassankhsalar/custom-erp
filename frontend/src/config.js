60b894b1b6ecac7a44d9fa8df2ec2edadc9e2109
Kh Salar Hassan <khsalarhassan@gmail.com>
2026-05-19 13:18:36 +0600

    deploy

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,8 @@
-export const API_BASE_URL = 'http://localhost:3001/api';
-export const MEDIA_BASE_URL = 'http://localhost:3001';
+
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001';
+
+export const API_BASE_URL = `${BASE_URL}/api`;
+export const MEDIA_BASE_URL = BASE_URL;
 // export const API_BASE_URL = 'https://inventoryback.taftiq.com/api';
 // export const MEDIA_BASE_URL = 'https://inventoryback.taftiq.com';
 
